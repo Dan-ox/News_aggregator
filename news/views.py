c11a4e977f1f29b5d79b0a04fc49e68070332193
dan <daniyar.kaisan@gmail.com>
2021-01-05 13:13:13 +0600

    fixed virtual environment

--- news/views.py
+++ news/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+bbc = 'https://www.bbc.com/'
+cons = 'https://www.builderonline.com/'
+sil = 'https://www.siliconvalley.com/'
+
+
+def get_bbc():
+    r = requests.get(bbc)
+    soup = bs(r, 'lxml')
+    posts = soup.find_all('div', 'class: media-list')
+    print(posts)
+
+
+get_bbc()
 
 
 def home(request):

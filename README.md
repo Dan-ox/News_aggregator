2c9544d094a6c9f1d9740c418e49cb101bb80dd8
dan <dtoks@mail.ru>
2021-01-06 21:19:48 +0600

    finished project

---
+++ README.md
@@ -0,0 +1,4 @@
+News aggregator. Takes news from three news sites: 'hacker news', 'tengri news', 'kolesa news'.
+For implementing this project I used django framework, bootstrap, html, lxml parser, beautifulsoup,
+"requests" and other modules.
+
--- news/views.py
+++ news/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup as Bs
-import lxml
 
 
-bbc = 'https://www.bbc.com/news/'
-cons = 'https://www.builderonline.com/'
-sil = 'https://www.siliconvalley.com/'
 hn = 'https://thehackernews.com/'
+tn = 'https://tengrinews.kz/world_news/'
+kolesa_news = 'https://kolesa.kz/content/'
 
 
 def get_hn():
@@ -23,6 +21,36 @@ def get_hn():
     return hn_list
 
 
+def get_tn():
+    tn_list = []
+    r = requests.get(tn).text
+    soup = Bs(r, 'lxml')
+    posts = soup.find_all('div', {'class': 'tn-article-item'})
+    for post in posts:
+        rough_title = post.find('span', {'class': 'tn-article-title'})
+        rough_path = post.find('a', {'class': 'tn-link'})
+        if rough_title is not None and rough_path is not None:
+            title = rough_title.text
+            url = "https://tengrinews.kz/" + rough_path.get('href')
+            data = {'title': title, 'url': url}
+            tn_list.append(data)
+    return tn_list
+
+
+def get_kolesa_news():
+    kn_list = []
+    r = requests.get(kolesa_news).text
+    soup = Bs(r, "lxml")
+    section = soup.find('section', {'id': 'main-list'})
+    posts = section.find_all('li')
+    for post in posts:
+        title = post.find('h2').text
+        url = "https://kolesa.kz/content/" + post.find('a').get('href')
+        data = {'title': title, 'url': url}
+        kn_list.append(data)
+    return kn_list
+
+
 def home(request):
-    context = {'news': get_hn()}
+    context = {'news_list': get_hn(), 'tn_list': get_tn(), 'kn_list': get_kolesa_news()}
     return render(request, 'news/home_page.html', context=context)

3e4f0f236fa92257188bb238ca47aa77fc60fe37
dan <dtoks@mail.ru>
2021-01-06 14:03:00 +0600

    changed email address

--- news/views.py
+++ news/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
 import requests
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as Bs
+import lxml
 
 
-bbc = 'https://www.bbc.com/'
+bbc = 'https://www.bbc.com/news/'
 cons = 'https://www.builderonline.com/'
 sil = 'https://www.siliconvalley.com/'
+hn = 'https://thehackernews.com/'
 
 
-def get_bbc():
-    r = requests.get(bbc)
-    soup = bs(r, 'lxml')
-    posts = soup.find_all('div', 'class: media-list')
-    print(posts)
-
-
-get_bbc()
+def get_hn():
+    hn_list = []
+    r = requests.get(hn).text
+    soup = Bs(r, "lxml")
+    posts = soup.find_all('div', {'class': 'body-post clear'})
+    for post in posts:
+        title = post.find('h2', {'class': 'home-title'}).text
+        url = post.find('a', {'class': 'story-link'}).get('href')
+        data = {'title': title, 'url': url}
+        hn_list.append(data)
+    return hn_list
 
 
 def home(request):
-    return render(request, 'news/home_page.html')
+    context = {'news': get_hn()}
+    return render(request, 'news/home_page.html', context=context)
